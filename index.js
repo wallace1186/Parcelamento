cd8d8c12f18201d7cd1ab0c3693731867c7a50fd
Nome Sobrenome <wallacewdsn@gmail.com>
2024-03-14 01:32:43 -0300

    correção2

--- index.js
+++ index.js
@@ -6,17 +6,22 @@ function calcularParcelas() {
       parteValor: prompt("Deseja dar um valor de entrada?")
    };
 
-   
+
    if (parcelamento.parteValor.toLowerCase() === "sim") {
       parcelamento.parteValor = parseInt(prompt("Quanto gostaria de adiantar?"));
       parcelamento.valorParcela = (parcelamento.valor - parcelamento.parteValor) / parcelamento.qtdParcelas;
    } else {
       parcelamento.valorParcela = parcelamento.valor / parcelamento.qtdParcelas;
    }
+   console.log(`Valor total de debito: ${parcelamento.valor}\nValor de entrada: ${parcelamento.parteValor}\nValor parcelado: ${parcelamento.valor - parcelamento.parteValor}`)
+
    for (let i = 0; i < parcelamento.qtdParcelas; i++) {
-      console.log(`Parcela ${i + 1}/${parcelamento.qtdParcelas} parcelas de R$ ${parcelamento.valorParcela.toFixed(2)}`);
-      alert(`Parcela ${i + 1}/${parcelamento.qtdParcelas} parcelas de R$ ${parcelamento.valorParcela.toFixed(2)}`);
+
+      console.log(`${parcelamento.qtdParcelas} parcelas de R$ ${parcelamento.valorParcela.toFixed(2)}`);
+      alert(`${parcelamento.qtdParcelas} parcelas de R$ ${parcelamento.valorParcela.toFixed(2)}`);
    }
+
+
 }
 
 calcularParcelas();
